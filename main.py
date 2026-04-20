a1631e61016c4b068d4d3e949753cbb079668c3f
IrtezaIshaq <150837618+irtazahup@users.noreply.github.com>
2026-04-20 08:05:32 +0500

    added docker file

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ async def med_chat(request: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
